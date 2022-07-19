6b9baf0558f808c77583a0367a367af647003502
Nistelq8 <aziz.almarzouq@gmail.com>
2022-07-19 20:38:41 +0300

    Solved task

--- pokemon/views.py
+++ pokemon/views.py
@@ -4,9 +4,10 @@ from .models import Pokemon
 
 def get_pokemon(request,pokemon_id):
     pokemon = Pokemon.objects.get(id=pokemon_id)
-    return HttpResponse(pokemon)
+    pokemon_info = f"{pokemon.name} - {pokemon.type} - {pokemon.hp}"
+    return HttpResponse(pokemon_info)
 
 def get_pokemons(request):
     pokemons = Pokemon.objects.all()
-    print(pokemons)
-    return HttpResponse(pokemons)
+    pokemon_list = "\n".join (f"{pokemon}" for pokemon in pokemons)
+    return HttpResponse(pokemon_list)
